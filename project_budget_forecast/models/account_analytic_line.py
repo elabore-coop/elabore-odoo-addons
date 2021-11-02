3bb7951e500ed749a69e0244b542925b7bf7cc10
Stéphan Sainléger <contact@stephansainleger.fr>
2021-11-02 21:54:44 +0100

    [UPD] Always add timesheet.so_line.product_id to analytic_lines

--- project_budget_forecast/models/account_analytic_line.py
+++ project_budget_forecast/models/account_analytic_line.py
@@ -7,6 +7,9 @@ class AccountAnalyticLine(models.Model):
     
     def _timesheet_preprocess(self, vals):
         vals = super(AccountAnalyticLine, self)._timesheet_preprocess(vals)
+        if vals.get('so_line') and not vals.get('product_id'):
+            so_line = self.env['sale.order.line'].browse(vals['so_line'])
+            vals['product_id'] = so_line.product_id.id
         if vals.get('employee_id') and not vals.get('product_id'):
             employee = self.env['hr.employee'].browse(vals['employee_id'])
             vals['product_id'] = employee.timesheet_product_id.id
